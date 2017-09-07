794ac7d31f4faacddd91f81024bfeec34fdd3504
jrzinck <jrzinck@gmail.com>
2017-09-07 14:01:02 -0500

    merged npm and webpacks together

--- react-redux-material_ui-boilerplate/webpack.config.js
+++ react-redux-material_ui-boilerplate/webpack.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  context: __dirname,
-  entry: {
-    jsx: "./src/index.jsx",
-    css: "./src/main.css",
-    html: "./src/index.html",
-  },
+    context: __dirname,
+    entry: {
+        jsx: "./src/index.jsx",
+        css: "./src/main.css",
+        html: "./src/index.html",
+    },
 
-  output: {
-    path: __dirname + "/static",
-    filename: "bundle.js",
-  },
-  module: {
-    preLoaders: [
-        //Eslint loader
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"},
-    ],
-    loaders: [
-      { test: /\.html$/, loader: "file?name=[name].[ext]" },
-      { test: /\.css$/, loader: "file?name=[name].[ext]" },
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
-    ],
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  eslint: {
-    configFile: './.eslintrc'
-  },
+    output: {
+        path: __dirname + "/static",
+        filename: "bundle.js",
+    },
+    module: {
+        preLoaders: [
+            //Eslint loader
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"},
+        ],
+        loaders: [
+            {test: /\.html$/, loader: "file?name=[name].[ext]"},
+            {test: /\.css$/, loader: "file?name=[name].[ext]"},
+            {test: /\.jsx?$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"]},
+        ],
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: "source-map",
+    eslint: {
+        configFile: './.eslintrc'
+    },
 };
